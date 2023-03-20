48ae9ec47060b7710129c87a435d904d6679cbbe
notKeallen <neca2609@gmail.com>
2023-03-20 16:38:49 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,11 +180,5 @@ subjectInput.addEventListener('input', function() {
       });
     }
 
-    const documentHeight = () => {
-      const doc = document.documentElement
-      doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
-     }
-     window.addEventListener('resize', documentHeight)
-     documentHeight()
 
 
