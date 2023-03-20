8e4b244585c15df3a465dbef8fc530f1f92137f7
notKeallen <neca2609@gmail.com>
2023-03-20 16:26:22 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,4 +180,11 @@ subjectInput.addEventListener('input', function() {
       });
     }
 
+    const documentHeight = () => {
+      const doc = document.documentElement
+      doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+     }
+     window.addEventListener('resize', documentHeight)
+     documentHeight()
+
 
